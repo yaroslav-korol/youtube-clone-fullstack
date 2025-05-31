bd6c7c210f53e726468a252585c12afbf88fd2a5
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-05-31 17:52:17 +0100

    Added auth button and logic to navbar

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,13 +1,36 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import SignInOut from "./sign-in-out";
+import { onAuthStateChangedHelper } from "../utils/firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+
 
 export default function Navbar() {
+    // Initialize user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    });
+
     return (
         <nav className={styles.navbar}>
             <Link href="/">
                 <Image src="/youtube-logo.svg" alt="Youtube logo" width={90} height={20}/> 
             </Link>
+            {
+                // TODO: Add an upload button
+            }
+            <SignInOut user={user} />
         </nav>
     );
 }
