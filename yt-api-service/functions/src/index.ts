3a6ddf9fc5b20f839b41dbd66ccbf550856f8bf5
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-05-31 23:23:28 +0100

    Added function for saving user data in Firestore Db

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,7 +1,21 @@
-import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as functions from "firebase-functions/v1";
 import * as logger from "firebase-functions/logger";
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
 });
