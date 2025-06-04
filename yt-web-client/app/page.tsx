6ec1396287fc6ccdb0d2bc2ed72d77212b24ebc5
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-06-04 21:04:08 +0100

    Added functionality for fetching and rendering videos on the main page

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -1,15 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./page.module.css";
+import { getVideos } from "./utils/firebase/functions";
 
-export default function Home() {
+export default async function Home() {
+  const videos = await getVideos();
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-        </ol>
+      <main>
+        {
+          videos.map((video) => (
+            <Link href={`/watch?v=${video.filename}`} key={video.id}>
+              <Image src={'/thumbnail.png'} alt='video' width={120} height={80} 
+                className={styles.thumbnail} />
+            </Link>
+          ))
+        }
       </main>
     </div>
   );
