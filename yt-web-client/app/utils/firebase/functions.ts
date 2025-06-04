0345d5b88c1acd389fcfa085a8b27456084b8060
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-06-04 20:59:45 +0100

    Added getVideo function and Video interface to next.js app

--- yt-web-client/app/utils/firebase/functions.ts
+++ yt-web-client/app/utils/firebase/functions.ts
@@ -1,9 +1,23 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase";
 
 
-const functions = getFunctions();
 const generateUploadUrlFunction = httpsCallable(functions, 'generateUploadUrl');
+const getVideosFunction = httpsCallable(functions, 'getVideos');
 
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: 'processing' | 'processed',
+  title?: string,
+  description?: string  
+}
+
+export async function getVideos() {
+  const response: any = await getVideosFunction();
+  return response.data as Video[];
+}
 
 export async function uploadVideo(file: File) {
     const response: any = await generateUploadUrlFunction({
