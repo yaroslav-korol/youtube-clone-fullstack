d28f0907bff27705187b6a51afddcfa400805c5a
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-06-11 17:31:20 +0100

    Fixed build error by wrapping 'useSearchParams' call into <Suspense> to allow it to defer rendering at runtime

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,18 +1,25 @@
 'use client';
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 
-export default function WatchPage() {
-    const videoPrefix = 'https://storage.googleapis.com/yk-youtube-processed-videos/';
-    const videoSrc = useSearchParams().get('v');
+function WatchContent() {
+  const videoPrefix = 'https://storage.googleapis.com/yk-youtube-processed-videos/';
+  const videoSrc = useSearchParams().get('v');
 
-    return (
-        <div>
-            <h1>Watch Page</h1>
-            <video controls src={`${videoPrefix}${videoSrc}`}/>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Watch Page</h1>
+      <video controls src={`${videoPrefix}${videoSrc}`} />
+    </div>
+  );
 }
 
-export const revalidate = 30;
+export default function WatchPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchContent />
+    </Suspense>
+  );
+}
